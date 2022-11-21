8b2aa5cc1dc44343517deb1545b20abb79a51f20
Alhivee <103689996+Alhivee@users.noreply.github.com>
2022-11-21 11:43:39 -0600

    iny

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
 
-$conexion=mysqli_connect("localhost","salon_tareas","Rodolfo123*","admonbd")or die(
+$conexion=mysqli_connect("localhost" , "softteci_paqueteria" ,"equipoweb","softteci_paqueteria")or die(
     "error de conexion");
 ?>
