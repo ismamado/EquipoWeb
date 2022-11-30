1c72d99cd73c6a84d3a74b41f51e41885bea8267
Alhivee <103689996+Alhivee@users.noreply.github.com>
2022-11-29 18:31:29 -0600

    Update delete.php

--- crud/delete.php
+++ crud/delete.php
@@ -3,9 +3,9 @@
 include("conexion.php");
 $con=conectar();
 
-$cod_estudiante=$_GET['id'];
+$ID=$_GET['id'];
 
-$sql="DELETE FROM alumno  WHERE ncontrol='$cod_estudiante'";
+$sql="DELETE FROM PAQUETE  WHERE ID_GUIA='$ID'";
 $query=mysqli_query($con,$sql);
 
     if($query){
