4cecf5d6ac6c50f0750388ae4d5b8d8ea746e7ea
Alhivee <103689996+Alhivee@users.noreply.github.com>
2022-11-29 19:53:34 -0600

    Update actualizar.php

--- crud/actualizar.php
+++ crud/actualizar.php
@@ -26,7 +26,7 @@ $row=mysqli_fetch_array($query);
                 <div class="container mt-5">
                     <form action="update.php?tipo=<?php echo $tipo  ?>&idu=<?php echo $idu ?>" method="POST">
                     
-                                <input type="hidden" name="ID GUIA" value="<?php echo $row['ID_GUIA']  ?>">
+                                <input type="text" name="ID GUIA" value="<?php echo $row['ID_GUIA']  ?>">
                                 
                               <input type="text" class="form-control mb-3" name="calif" placeholder="calif" value="<?php echo $row['calif']  ?>">
                                 <input type="text" class="form-control mb-3" name="nombres" placeholder="Nombres" value="<?php echo $row['nombres']  ?>">
