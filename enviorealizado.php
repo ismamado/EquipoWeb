9129029177fb32e43c1aafc528aaa996f252f9c4
Alhivee <103689996+Alhivee@users.noreply.github.com>
2022-12-01 21:51:04 -0600

    Update enviorealizado.php

--- enviorealizado.php
+++ enviorealizado.php
@@ -55,9 +55,9 @@ if ($conexion) {
     $id = mysqli_insert_id($conexion);
  /* $cuerpo = "  
     HOLA $nombre_remitente! Con este correo confirmamos que realizaste un envio de paqueteria,  ¡Gracias por su compra! Su numero de Guia es: $id "  ;  */
-    $remitente = "FROM:". "envios@paqueteria.softtecisc.com";
-    $remitente .= 'MIME-Version: 1.0' . "\r\n";
-                          $remitente .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+   // $remitente = "FROM:". "envios@paqueteria.softtecisc.com";
+    $remitente = 'MIME-Version: 1.0' . "\r\n";
+                          $remitente .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n" . " FROM:". "envios@paqueteria.softtecisc.com";
    $cuerpo = ' <html lang="es">
                              <head> <meta charset="UTF-8" /> </head>
                               <body>
